1750a1af767fd19769e1d12e40503169b6cb3700
gustavo-sena-git <gustavo_sena-alves@estudante.sesisenai.org.br>
2026-06-15 11:15:06 -0300

    ADD - adicionei novas funções

--- acoes.php
+++ acoes.php
@@ -9,5 +9,7 @@ if (isset($_POST['create_usuario'])){
     $senha = isset($_POST ('senha')) ? mysqli_real_escape_string($conexao, trin($_POST['nome']))
 
     $sql = "INSERT INTO usuario (nome, email, data_nascimento, senha) VALUES ('$nome', '$email', '$data_nascimento', '$senha')";
+
+    mysqli_query($conexao, $sql); 
 }
 ?>
